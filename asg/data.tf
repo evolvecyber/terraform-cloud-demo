c2a6262a9b9a2bb247c38fa46f8ded2b4c09c0c7
Evolve Cyber LLC <fsadykov@evolvecyber.com>
2024-03-16 15:39:32 -0500

    added asg

---
+++ asg/data.tf
@@ -0,0 +1,44 @@
+
+# Pulls VPC related information
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "Sadykov123"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+# DB related information 
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "Sadykov123"
+    workspaces = {
+      name = "rds"
+    }
+  }
+}
+
+
+# Pulls AMI information
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
